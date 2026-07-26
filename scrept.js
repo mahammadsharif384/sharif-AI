be4f26ca9834b135d84dc064b3ca3e2bd20f274f
SHAIK MAHAMMAD SHARIF <mahammadsharif384@gmail.com>
2026-07-26 16:38:28 +0530

    Update scrept.js add

--- scrept.js
+++ scrept.js
@@ -4,6 +4,17 @@ let input=document.getElementById("in");
 let mic=document.getElementById("mic");
 let search=document.getElementById("search");
 let body=document.body;
+let profile=document.querySelector(".profile");
+let profilepic=document.getElementById("profilepic");
+let fileinput=document.getElementById("fileinput");
+let men=document.getElementById("men");
+let menubar=document.querySelector(".menubar");
+let name=document.getElementById("name");
+let email=document.getElementById("email");
+let username=localStorage.getItem("name");
+let useremail=localStorage.getItem("email");
+ name.innerText=username;
+ email.innerText=useremail;
 let bg="dark";
 p.forEach((pl)=> {
     pl.addEventListener("click",(e)=>{
@@ -59,3 +70,33 @@ recognition.onerror = (event)=>{
     alert("Error:"+event.error);
 }
 
+//profile input
+
+profilepic.addEventListener("click",()=>{
+   fileinput.click();
+})
+fileinput.addEventListener("change",()=>{
+   let file=fileinput.files[0];
+   if(file){
+      let reader=new FileReader();
+      reader.onload=function(e){
+         profilepic.src=e.target.result;
+      };
+      reader.readAsDataURL(file);
+   }
+})
+//dash board showing
+let count=0;
+men.addEventListener("click",hanif);
+function hanif(){
+  if(count == 0){
+    menubar.style.visibility="visible";
+    men.style.left="13em";
+    count =1;
+  }
+  else{
+    menubar.style.visibility="hidden";
+     men.style.left="0px";
+    count = 0;
+  }
+}
